a6d98e3b3a68ce062290aa4aa01bc37cf072b958
Alexey Savchuk <sabo@devpreview.ru>
2015-03-04 17:16:24 +0300

    Install / uninstall unique keys / indexes

--- src/UserStorageSchema.php
+++ src/UserStorageSchema.php
@@ -13,6 +13,7 @@ use Drupal\Core\Entity\Sql\SqlContentEntityStorage;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 use Drupal\Core\Entity\ContentEntityTypeInterface;
+use Drupal\Core\Database\SchemaObjectExistsException;
 
 /**
  * Defines the user schema handler.
@@ -82,13 +83,42 @@ class UserStorageSchema extends BaseUserStorageSchema implements UserStorageSche
   protected function installExistsTable($table_name, array $table_schema) {
     $schema_handler = $this->database->schema();
     $installed_fields = $this->keyValue()->get('installed_fields', array());
+    $installed_unique_keys = $this->keyValue()->get('installed_unique_keys', array());
+    $installed_indexes = $this->keyValue()->get('installed_indexes', array());
+
     foreach ($table_schema['fields'] as $field_name => $field_schema) {
       if (!$schema_handler->fieldExists($table_name, $field_name)) {
         $installed_fields[$table_name][] = $field_name;
         $schema_handler->addField($table_name, $field_name, $field_schema);
       }
     }
+
+    foreach ($table_schema['unique keys'] as $key_name => $fields) {
+      /** @see https://www.drupal.org/node/2445839 */
+      try {
+        $schema_handler->addUniqueKey($table_name, $key_name, $fields);
+        $installed_unique_keys[$table_name][] = $key_name;
+      }
+      catch (SchemaObjectExistsException $ex) {
+        
+      }
+    }
+
+    /**
+     * @todo: foreign keys
+     */
+    //foreach ($table_schema['foreign keys'] as $key_name => $fields) {}
+
+    foreach ($table_schema['indexes'] as $index_name => $fields) {
+      if (!$schema_handler->indexExists($table_name, $index_name)) {
+        $installed_indexes[$table_name][] = $index_name;
+        $schema_handler->addIndex($table_name, $index_name, $fields);
+      }
+    }
+
     $this->keyValue()->set('installed_fields', $installed_fields);
+    $this->keyValue()->set('installed_unique_keys', $installed_unique_keys);
+    $this->keyValue()->set('installed_indexes', $installed_indexes);
   }
 
   /**
@@ -96,14 +126,29 @@ class UserStorageSchema extends BaseUserStorageSchema implements UserStorageSche
    */
   public function uninstall() {
     $schema_handler = $this->database->schema();
-    foreach ($this->keyValue()->get('installed_tables', array()) as $table_name) {
-      $schema_handler->dropTable($table_name);
+
+    foreach ($this->keyValue()->get('installed_indexes', array()) as $table_name => $indexes) {
+      foreach ($indexes as $indexe_name) {
+        $schema_handler->dropIndex($table_name, $indexe_name);
+      }
     }
+
+    foreach ($this->keyValue()->get('installed_unique_keys', array()) as $table_name => $keys) {
+      foreach ($keys as $key_name) {
+        $schema_handler->dropUniqueKey($table_name, $key_name);
+      }
+    }
+
     foreach ($this->keyValue()->get('installed_fields', array()) as $table_name => $fields) {
       foreach ($fields as $field_name) {
         $schema_handler->dropField($table_name, $field_name);
       }
     }
+
+    foreach ($this->keyValue()->get('installed_tables', array()) as $table_name) {
+      $schema_handler->dropTable($table_name);
+    }
+
     $this->keyValue()->deleteAll();
   }
 
